9d74a08d9c1fbee0a342654c52301024d1c69bf3
Lukáš Fridmanský <luky.fridmansky@gmail.com>
2023-10-31 10:05:58 +0100

    fix: image loaded

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -33,7 +33,7 @@ const StoryImage: FC<StoryImageProps> = ( {
 
     }
 
-    if ( data.uri === story.sourceUrl && ( data.isVideo || videoDuration.value !== undefined ) ) {
+    if ( data.uri === story.sourceUrl && ( !data.isVideo || videoDuration.value !== undefined ) ) {
 
       onLoad( videoDuration.value );
 
